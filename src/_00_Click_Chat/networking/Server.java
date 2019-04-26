8475c966b67673c62d66d618b65657fd9c1d2735
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-04-25 22:17:55 -0700

    done

--- src/_00_Click_Chat/networking/Server.java
+++ src/_00_Click_Chat/networking/Server.java
@@ -8,15 +8,12 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 import javax.swing.JOptionPane;
 
 public class Server {
 	private int port;
-
 	private ServerSocket server;
 	private Socket connection;
-
 	ObjectOutputStream os;
 	ObjectInputStream is;
 
@@ -24,32 +21,18 @@ public class Server {
 		this.port = port;
 	}
 
-	public void start(){
+	public void start() {
 		try {
 			server = new ServerSocket(port, 100);
-
 			connection = server.accept();
-
 			os = new ObjectOutputStream(connection.getOutputStream());
 			is = new ObjectInputStream(connection.getInputStream());
-
 			os.flush();
-
-			while (connection.isConnected()) {
-				try {
-					JOptionPane.showMessageDialog(null, is.readObject());
-					System.out.println(is.readObject());
-				}catch(EOFException e) {
-					JOptionPane.showMessageDialog(null, "Connection Lost");
-					System.exit(0);
-				}
-			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getIPAddress() {
 		try {
 			return InetAddress.getLocalHost().getHostAddress();
