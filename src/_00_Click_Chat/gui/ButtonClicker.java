561a3e0f502468bc827cd59895260db47e25b33b
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-04-25 22:16:45 -0700

    done

--- src/_00_Click_Chat/gui/ButtonClicker.java
+++ src/_00_Click_Chat/gui/ButtonClicker.java
@@ -3,29 +3,27 @@ package _00_Click_Chat.gui;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
 import _00_Click_Chat.networking.Client;
 import _00_Click_Chat.networking.Server;
 
 public class ButtonClicker extends JFrame {
 	JButton button = new JButton("CLICK");
-	
 	Server server;
 	Client client;
-	
-	
+
 	public static void main(String[] args) {
 		new ButtonClicker();
 	}
-	
-	public ButtonClicker(){
-		
-		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
-		if(response == JOptionPane.YES_OPTION){
+
+	public ButtonClicker() {
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!",
+				JOptionPane.YES_NO_OPTION);
+		if (response == JOptionPane.YES_OPTION) {
 			server = new Server(8080);
 			setTitle("SERVER");
-			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
-			button.addActionListener((e)->{
+			JOptionPane.showMessageDialog(null,
+					"Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			button.addActionListener((e) -> {
 				server.sendClick();
 			});
 			add(button);
@@ -33,14 +31,13 @@ public class ButtonClicker extends JFrame {
 			setSize(400, 300);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			server.start();
-			
-		}else{
+		} else {
 			setTitle("CLIENT");
 			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
 			String prtStr = JOptionPane.showInputDialog("Enter the port number");
 			int port = Integer.parseInt(prtStr);
 			client = new Client(ipStr, port);
-			button.addActionListener((e)->{
+			button.addActionListener((e) -> {
 				client.sendClick();
 			});
 			add(button);
